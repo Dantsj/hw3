42ccb1eb779422e1f1524600afea33c995e83898
Daniel Tsypkin <daniel.tsypkin@kellogg.northwestern.edu>
2024-05-13 04:04:33 +0000

    Done

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,17 @@
 class EntriesController < ApplicationController
+  def new
+    @place = Place.find_by({ "id" => params["place_id"] })
+  end
+
+  def create
+    @entry = Entry.new
+    @entry["title"] = params["title"]
+    @entry["description"] = params["description"]
+    @entry["posted_on"] = Date.today
+    @entry["place_id"] = params["place_id"]
+    @entry.save
+    redirect_to "/places/#{@entry["place_id"]}"
+end
+
+
 end
--- db/migrate/20240513004153_create_journal_entries.rb
+++
@@ -1,8 +0,0 @@
-class CreateJournalEntries < ActiveRecord::Migration[7.1]
-  def change
-    create_table :journal_entries do |t|
-
-      t.timestamps
-    end
-  end
-end
